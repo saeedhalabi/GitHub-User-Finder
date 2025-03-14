1258bbd54499295a67cf556b2bdffcdeb89c1883
Saeed Halabi <saeedhalabi80@gmail.com>
2025-03-14 12:32:58 +0200

    Add project title

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main>
+      <h1 className="text-center py-6 font-bold text-3xl">
+        GitHub User Finder
+      </h1>
+    </main>
+  );
 };
 
 export default App;
